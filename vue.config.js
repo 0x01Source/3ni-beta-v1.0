b0d09a9d5fafe81246f1df2d04ec69612ad1aed0
Mostafa Abdel Naeem <97797163+MAbdelnaeem50@users.noreply.github.com>
2024-08-26 22:57:38 +0300

    Beta-v1.0

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/3ni-Beta/' : '/',
+
   devServer: {
     port: 8090,
   },
