33812a77154ece90e5f9f9d57895dae3f4017bf7
Mostafa Abdel Naeem <97797163+MAbdelnaeem50@users.noreply.github.com>
2024-08-26 23:40:46 +0300

    Deploy Vue project

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/vue3ni/' // Replace with your repository name
-  : '/',
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/3ni-Beta/' : '/',
   devServer: {
     port: 8090,
   },
   transpileDependencies: true,
   lintOnSave: false,
-};
+});
